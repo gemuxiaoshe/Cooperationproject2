fcfa22f7c3c402b0f0eaf43315086e80b41f4525
thewinding <1528307417@qq.com>
2019-05-09 13:21:48 +0800

    第一次粗略登录雏形

--- app/src/main/java/cn/gemuxiaoshe/cooperationproject/MainActivity.java
+++ app/src/main/java/cn/gemuxiaoshe/cooperationproject/MainActivity.java
@@ -2,6 +2,10 @@ package cn.gemuxiaoshe.cooperationproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item_add:
+                Toast.makeText(this, "你点击了“用户”按键！", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
 }
+
